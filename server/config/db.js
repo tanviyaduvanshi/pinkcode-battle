64498fe20312826f721df7588b458d1272e2d5b5
Tanvi Yaduvanshi <tanvithakur1215@gmail.com>
2026-04-10 18:55:40 +0530

    fixed mongodb connection issue

--- server/config/db.js
+++ server/config/db.js
@@ -5,10 +5,8 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    const conn = await
+      mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(`Error connection to MongoDB: ${err.message}`);
